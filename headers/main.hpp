6fee4e708e9361a5912c3e77a28970624caa7a4d
Jarrod Burns <87616660+JarrodBurns@users.noreply.github.com>
2022-04-15 12:21:52 -0500

    Add files via upload
    ### General
    
    - minor refactoring and enhancements.
    - Added header directory and moved all includes to a new header file, "main.hpp"
    
    ### Bug Fixes
    Corrected an errant variable assignment which was causing a file to remain open indefinitely if the user did not have a "last_count.txt" file in the same directory as the application.

---
+++ headers/main.hpp
@@ -0,0 +1,9 @@
+
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <windows.h>
--- main.cpp
+++ main.cpp
@@ -2,14 +2,13 @@
 
 // @autor: Jarrod Burns
 // @github: github.com/JarrodBurns
-// @version 1.0.0   -   04/12/2021 
+// @version 1.0.2   -   04/14/2022 
 
 
-#include <iostream>
-#include <string>
-#include "GFX.hpp"
-#include "DCount.hpp"
-#include "FileHandler.hpp"
+#include "headers\main.hpp"
+#include "headers\GFX.hpp"
+#include "headers\DCount.hpp"
+#include "headers\FileHandler.hpp"
 
 
 int main()
@@ -17,18 +16,20 @@ int main()
     GFX Graphics;
     DCount App;
     FileHandler FileManager;
-    const std::string file_name = "lst.txt";
+
+    const std::string file_name = "last_count.txt";
+
     std::string command;
     int count = FileManager.read_from_file(file_name);
 
     // first time setup
     App.set_from_file_history(count);
     Graphics.build_frame();
-    Graphics.set_place_values(App.place_values);
+    Graphics.set_segment_values(App.segment_values);
     Graphics.clear_screen();
     Graphics.draw_screen();
 
-
+ 
     // main loop
     while(true)
     {
@@ -36,11 +37,11 @@ int main()
         App.deviate_count(command);
 
         Graphics.set_previous_command(App.previous_command);
-        Graphics.set_place_values(App.place_values);
+        Graphics.set_segment_values(App.segment_values);
         Graphics.clear_screen();
         Graphics.draw_screen();
 
-        int count_history = App.get_count();
+        int count_history = App.count;
         FileManager.write_to_file(file_name, count_history);
     }
 }
