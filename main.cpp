f33e24d5c2c1606d18c5a3b6d019dfe067a4a5b3
Jarrod Burns <87616660+JarrodBurns@users.noreply.github.com>
2022-04-17 11:13:57 -0500

    minor
    ## General
    - Modified the FileHandler.h to write a .dat file to the users *appdata/local* directory under */dCount/value.dat*. This should eliminate the potential for an orphaned *.txt* file, should the the user place the *.exe* on their desktop.
    
    ## Build
    - Upgraded from C++ 11 to C++ 17
    - Switched compiler to MSVC

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 
 // @autor: Jarrod Burns
 // @github: github.com/JarrodBurns
-// @version 1.0.2   -   04/14/2022 
+// @version 1.0.3   -   04/17/2022 
 
 
-#include "headers\main.hpp"
-#include "headers\GFX.hpp"
-#include "headers\DCount.hpp"
-#include "headers\FileHandler.hpp"
+#include "dCount\main.h"
+#include "dCount\GFX.h"
+#include "dCount\DCount.h"
+#include "dCount\FileHandler.h"
 
 
 int main()
@@ -17,21 +17,19 @@ int main()
     DCount App;
     FileHandler FileManager;
 
-    const std::string file_name = "last_count.txt";
-
     std::string command;
-    int count = FileManager.read_from_file(file_name);
+    int count = FileManager.read_count_history();
 
     // first time setup
-    App.set_from_file_history(count);
+    App.set_from_count_history(count);
     Graphics.build_frame();
     Graphics.set_segment_values(App.segment_values);
     Graphics.clear_screen();
     Graphics.draw_screen();
 
- 
+
     // main loop
-    while(true)
+    while (true)
     {
         std::cin >> command;
         App.deviate_count(command);
@@ -42,6 +40,6 @@ int main()
         Graphics.draw_screen();
 
         int count_history = App.count;
-        FileManager.write_to_file(file_name, count_history);
+        FileManager.write_count_history(count_history);
     }
 }
