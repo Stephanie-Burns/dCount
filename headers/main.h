01990ad9f8b05c350839a7d4cfc07de79d5df82a
Jarrod Burns <87616660+JarrodBurns@users.noreply.github.com>
2022-04-17 11:18:01 -0500

    minor
    updated for new includes

---
+++ headers/main.h
@@ -0,0 +1,12 @@
+
+#include <algorithm>
+#include <cmath>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#include <windows.h>
+#include <shlobj.h>
