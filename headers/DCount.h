4fde644c720c2f4fe30ab62450830ce85424179d
Jarrod Burns <87616660+JarrodBurns@users.noreply.github.com>
2022-04-18 16:54:32 -0500

    minor
    ## General
    - The .deviate_count() method was a little breathy, the syntax has been simplified with use of the ternary operator.
    - The .parse_user_input() method has been removed. User input is now checked against a map of prepared commands.

--- headers/DCount.h
+++ headers/DCount.h
@@ -7,13 +7,20 @@
 class DCount
 {
 private:
-    const std::vector<std::string> special_commands =
-    { "R", "-", "*", "/", "**", "//" };
+    std::map<std::string, int> command_list =
+    {
+        { "R",   1 },       // Reset
+        { "-",   2 },       // Sub (1)
+        { "*",   3 },       // Add (10)
+        { "/",   4 },       // Sub (10)
+        { "**",  5 },       // Add (100)
+        { "//",  6 }        // Sub (100)
+    };
 
 
     void set_segment_values()
-        // Converts a number into it's place values and assigns them accordingly.
-        // For count < 10,000 | count = 905, Result: 0, 9, 0, 5
+    // Converts a number into it's place values and assigns them accordingly.
+    // For count < 10,000 | count = 905, Result: 0, 9, 0, 5
     {
         // convert to string and pad to 4 digits
         std::string i = std::to_string(count);
@@ -26,28 +33,12 @@ private:
         }
 
         // assignment
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 4; ++i)
         {
             segment_values[i] = count_repr[i] - '0';
         }
     }
 
-
-    int parse_user_input(std::string user_input)
-        // Checks user input against a predefined list of commands.
-        // Registered commands: 'R', '-', '/', '*', "//", "**"
-        // If successful, the index of that command will be returned.
-    {
-        auto iter = find(special_commands.begin(), special_commands.end(),
-            user_input);
-
-        if (iter != special_commands.end())
-        {
-            int index = iter - special_commands.begin();
-            return index;
-        }
-        return -1;
-    }
 
 public:
     std::vector<int> segment_values = { 0, 0, 0, 0 };
@@ -56,9 +47,9 @@ public:
 
 
     void set_from_count_history(int count_history)
-        // Setter for count from file history.
-        // Intended for use with file history, to set
-        // a saved value from the last session.
+    // Setter for count from file history.
+    // Intended for use with file history, to set
+    // a saved value from the last session.
     {
         count = count_history;
         set_segment_values();
@@ -72,39 +63,39 @@ public:
 
 
     void deviate_count(std::string user_input)
-        // Increments or decrements the count based on user input.
-        // Range 0 < 10,000
+    // Increments or decrements the count based on user input.
+    // Range 0 < 10,000
     {
-        int i = parse_user_input(user_input);
+        int i = command_list[user_input];
 
         switch (i)
         {
-        case 0:     // Reset
+        case 1:     // Reset
             set_previous_command("R");
             count = 0;
             break;
 
-        case 1:     // Sub (1)
+        case 2:     // Sub (1)
             set_previous_command("-");
             count < 1       ?   count = 0   :   --count;
             break;
 
-        case 2:     // Add (10)
+        case 3:     // Add (10)
             set_previous_command("*");
             count > 9998    ?  count = 9999 :   count += 10;
             break;
 
-        case 3:     // Sub (10)
+        case 4:     // Sub (10)
             set_previous_command("/");
             count < 10      ?  count = 0    :   count -= 10;
             break;
 
-        case 4:     // Add (100)
+        case 5:     // Add (100)
             set_previous_command("**");
             count > 9900    ?  count = 9999 :   count += 100;
             break;
 
-        case 5:     // Sub (100)
+        case 6:     // Sub (100)
             set_previous_command("//");
             count < 100     ?  count = 0    :   count -= 100;
             break;
